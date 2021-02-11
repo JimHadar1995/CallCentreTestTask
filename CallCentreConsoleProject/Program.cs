582b8a78e2f42927d3dc3979d5e4ec34697916d8
Kudashev <dima-kudachev1995@mail.ru>
2021-02-12 01:18:39 +0300

    catch in main

--- CallCentreConsoleProject/Program.cs
+++ CallCentreConsoleProject/Program.cs
@@ -9,13 +9,20 @@ namespace CallCentreConsoleProject
 
         static void Main(string[] args)
         {
-            string testFile = args.Length > 0 ? args[0] : DefaultTestFile;
+            try
+            {
+                string testFile = args.Length > 0 ? args[0] : DefaultTestFile;
 
-            var worker = new CallCentreWorker(testFile);
+                var worker = new CallCentreWorker(testFile);
 
-            worker.PrintPart1();
+                worker.PrintPart1();
 
-            //worker.PrintPart2();
+                worker.PrintPart2();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"При работе произошла ошибка: {ex.Message}");
+            }
 
             Console.ReadKey();
         }
